08f7f5a227960c0f2f0e52d1fd7f6436920ae062
William Casarin <william@casarin.me>
2013-09-30 22:39:00 -0700

    fix notify-client and added from

--- notify-client.js
+++ notify-client.js
@@ -6,24 +6,25 @@ var argv = require('optimist')
     .demand(['t', 'm'])
     .argv;
 
-var target = argv.t || process.argv[2];
-var msg = argv.m || process.argv[3];
+var target = argv.t;
+var msg = argv.m;
+var from = argv.f;
 var timeout = argv.timeout || 1000;
 
 var split = target.split('@');
 var domain = split[1];
-var to = split[0];
 
 query({ name: 'notify', domain: domain }, function (err, data) {
   data = data[0];
   var client = net.connect(data.port, data.name, function (c) {
     var packet = {
-      to: to,
+      to: target,
+      from: from,
       msg: msg
     };
 
     console.log("Sending notification to '" + target + "' ...");
-    c.write(JSON.stringify(packet));
+    client.write(JSON.stringify(packet));
   });
 
   client.setTimeout(timeout, function() {
